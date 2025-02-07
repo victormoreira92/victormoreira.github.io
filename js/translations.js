d136218be9e146949156ef6852ce62b228618f1b
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-02-07 12:04:48 -0300

    feat: Nova pagiina de portifolio

---
+++ js/translations.js
@@ -0,0 +1,50 @@
+export const translations = {
+  en: {
+    title: "Welcome",
+    introduce_text: "I'm Victor Moreira",
+    introduce_content_text: "Software developer with experience in Ruby on Rails. Passionate about technology and best development practices, I apply methodologies like TDD and BDD to ensure high-quality code.",
+    skill_title: 'Skills',
+    skills_subtitle: 'My skills', 
+    skills_description: 'With experience in backend and frontend development, automated testing, databases, and infrastructure, I build efficient and impactful applications.',
+    skill_banco_dados: 'Database',
+    skill_versionamento: 'Version Control',
+    portifolio_title: "Check Out Some of My Works.",
+    portifolio_subtitle: "A collection of projects, including web applications and APIs, that I have developed",
+    micolab_description: "Clinical laboratory management system, covering everything from sample and patient registration to exam result release.",
+    micolab_status: "Developement",
+    xml_flow_description: "System for processing XML files, allowing automated import, validation, and data extraction.",
+    xml_flow_status: "Developement",
+    validata_description: "Application that performs document validation and automated personal data generation securely and efficiently",
+    validata_status: "Developement",
+    deezer2youtube_description: "API that allows to transfer Deezer playlists to YouTube. Provide the link Deezer playlist, and the system will find the corresponding songs on YouTube, creating a new playlist.",
+    deezer2youtube_status: "Developement"
+
+  },
+  pt: {
+    title: "Bem-vindo",
+    introduce_text: "Eu sou Victor Moreira",
+    introduce_content_text: "Desenvolvedor de software com experiência em Ruby on Rails. Apaixonado por tecnologia e boas práticas de desenvolvimento, aplico metodologias como TDD e BDD para garantir código de qualidade.",
+    portifolio_title: "Veja alguns dos meus trabalhos",
+    skill_title: 'Habilidades',
+    skill_banco_dados: 'Banco de Dados',
+    skill_versionamento: 'Versionaento',
+    skills_subtitle: 'O que eu domino', 
+    skills_description: 'Com experiência em desenvolvimento backend e frontend, testes automatizados, bancos de dados e infraestrutura, para criar aplicações eficientes e úteis.',
+    portifolio_subtitle: "Uma coleção de projetos, incluindo aplicações web e APIs, que desenvolvi.",
+    micolab_description: "Sistema de gerenciamento para laboratórios clínicos, abrangendo desde o cadastro de amostras e pacientes até a liberação de resultados de exames.",
+    micolab_status: "Em desenvolvimento",
+    xml_flow_description: "Sistema para processamento de arquivos XML, permitindo a importação, validação e extração de dados de forma automatizada.",
+    xml_flow_status: "Em desenvolvimento",
+    validata_description: "Aplicação que realiza a validação de documentos e a geração de dados pessoais de forma automatizada e segura.",
+    validata_status: "Em desenvolvimento",
+    deezer2youtube_description: "API que permite transferir playlists do Deezer para o YouTube. Basta fornecer o link da playlist do Deezer, e o sistema buscará as músicas correspondentes no YouTube, criando uma nova playlist.",
+    deezer2youtube_status: "Em desenvolvimento"
+  
+  }
+};
+
+export const elementos =[
+  'title','introduce_text','introduce_content_text','portifolio_title', 'portifolio_subtitle', 'micolab_description', 'micolab_status', 'xml_flow_description', 'xml_flow_status',
+  'validata_description', 'validata_status','skill_banco_dados', 'deezer2youtube_description', 'deezer2youtube_status', 'skill_title','skills_subtitle', 'skills_description',
+  'skill_banco_dados','skill_versionamento'
+]
